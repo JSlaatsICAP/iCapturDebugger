d39fd36af43532d77dfc768c1130d0256051d31f
Joery Slaats <you@example.com>
2021-03-05 14:43:00 +0100

    changed standard data folder

--- iCaptur_Debug.py
+++ iCaptur_Debug.py
@@ -2,8 +2,9 @@ import iCaptur
 import json
 
 filename = input('enter filename\n')
+fileloc = "data/" + filename
 exercise = input('enter excercise (squat,lunge, birddog, bridge or plank)\n')
-with open(filename) as input_file:
+with open(fileloc) as input_file:
         data = json.load(input_file)
 
 iCaptur.analyze(data, exercise)
